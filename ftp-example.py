47f0ad5c94a22bd483a330d5cda98a6d835adfdd
haris <hhaarree@hotmail.com>
2017-10-02 17:53:38 +0200

    First commit

---
+++ ftp-example.py
@@ -0,0 +1,29 @@
+import ftplib
+from ftplib import FTP
+import os
+
+ip, port = input("Enter the IP address and the port, separated by a space: ").split(" ")
+
+server = FTP()
+server.connect(ip, int(port))
+server.login()
+
+server.retrlines("LIST")
+
+print(os.getcwd())
+
+filename = input("Enter the name of the file you want to download: ")
+
+
+while True:
+    try:
+        server.retrbinary("RETR " + filename, open(filename, "wb").write)
+        break
+    except ftplib.all_errors as e:
+        print(e)
+        filename = input("Try again: ")
+        continue
+
+
+
+
